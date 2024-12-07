6f2fe59b1dcecf5be7ded7c37c6abe71fb3f10fb
valenfiumana <valen.fiumana@gmail.com>
2024-12-07 16:05:19 -0300

    Agregando modals y quienes somos

--- script.js
+++ script.js
@@ -9,32 +9,42 @@ const toggleNav = () => {
 };
 mobileNav.addEventListener("click", () => toggleNav());
 
-const openEls = document.querySelectorAll("[data-open]");
-const closeEls = document.querySelectorAll("[data-close]");
-const isVisible = "is-visible";
 
-for (const el of openEls) {
-  el.addEventListener("click", function () {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-  });
+/* MODAL */
+/*modal 1 */
+function openModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
 }
 
-for (const el of closeEls) {
-  el.addEventListener("click", function () {
-    this.parentElement.parentElement.classList.remove(isVisible);
-  });
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
 
-document.addEventListener("click", (e) => {
-  if (e.target == document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+window.onclick = function (event) {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
   }
-});
+};
 
-document.addEventListener("keyup", (e) => {
-  // if we press the ESC
-  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
-  }
-});
+/*modal */
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
